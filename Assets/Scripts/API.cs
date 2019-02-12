e20aa166b949105e54951e721555596883ee58f2
Christian Rhodes <christian.rhodes@oit.edu>
2019-02-12 10:24:07 -0800

    Hello. I am Boreas. Your friendly VR Weather App and this is my first commit ever!

---
+++ Assets/Scripts/API.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class API : MonoBehaviour
+{
+    private const string url =
+        "http://api.openweathermap.org/data/2.5/weather?q=Wilsonville,US&appid=272d4a7147e89ee64c76e699bdfa2a9e&units=&cnt=6";
+    
+    // Use this for initialization
+    public void Request () {
+        WWW request = new WWW(url);
+        StartCoroutine(OnResponse(request));
+        
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+        yield return req;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SceneMgmt.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgmt : MonoBehaviour
+{
+    public void SceneSwitcher()
+    {
+        System.DateTime currentTime;
+        var now = currentTime.TimeOfDay;
+
+        System.TimeSpan morningStart = new System.TimeSpan(07, 0, 0);
+        System.TimeSpan morningEnd = new System.TimeSpan(11, 59, 59);
+
+        System.TimeSpan afternoonStart = new System.TimeSpan(12, 0, 0);
+        System.TimeSpan afternoonEnd = new System.TimeSpan(16, 59, 59);
+
+        System.TimeSpan eveningStart = new System.TimeSpan(17, 0, 0);
+        System.TimeSpan eveningEnd = new System.TimeSpan(20, 59, 59);
+
+        if ((now > morningStart) && (now < morningEnd))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        else if ((now > afternoonStart) && (now < afternoonEnd))
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        else if ((now > eveningStart) && (now < eveningEnd))
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        else
+        {
+            SceneManager.LoadScene(4);
+        }
+
+    }
+}
